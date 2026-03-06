e7e14041e13d4a0ef1990ea3f20d9a460c48c83d
ferty460 <rostovcevvladimir460@gmail.com>
2026-03-06 20:44:12 +0300

    initial commit

---
+++ src/test/java/org/example/filestorage/FileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.filestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
