1f50380cc619016656ab93815c1deb32f3f158aa
ferty460 <rostovcevvladimir460@gmail.com>
2026-04-04 13:53:26 +0300

    feat: configure session storage inside Redis

--- src/main/java/org/example/filestorage/FileStorageApplication.java
+++ src/main/java/org/example/filestorage/FileStorageApplication.java
@@ -2,8 +2,10 @@ package org.example.filestorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class FileStorageApplication {
 
     public static void main(String[] args) {
